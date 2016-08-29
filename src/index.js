b7f7a86be976ed6a7ac4d0cc84b202c3bf66c2d4
vitalets <vitalets@yandex-team.ru>
2016-08-29 21:13:10 +0300

    simple sample

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@
 
 const fileserver = require('selenium-webdriver/lib/test/fileserver');
 
-console.log('Starting server on ' + process.env.PORT);
-
+/*
 fileserver.start(process.env.PORT || 2311).then(function() {
     console.log('Server running at ' + fileserver.url());
+});
+*/
+
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+  var result = 'App is running';
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
 });
