95d4f0c9be9e16af5901b115cbd59c1149d0a4d0
vitalets <vitalets@yandex-team.ru>
2016-08-29 22:49:39 +0300

    remove spaces and comments

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@
  heroku cant start fileserver as is because it passes 'localhost' to .listen() method.
  To overcome that with no edits of original httpserver.js we intercept http.createServer and exclude host
  */
-
 const http = require('http');
 const _createServer = http.createServer;
 http.createServer = function () {
@@ -23,18 +22,3 @@ const fileserver = require('selenium-webdriver/lib/test/fileserver');
 fileserver.start(process.env.PORT || 2311).then(function() {
   console.log('Server running at ' + fileserver.url());
 });
-
-/*
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-  var result = 'App is running';
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
-});
-*/
