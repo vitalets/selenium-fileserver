8295766d113b3b05bfeab07174a18e3443ca53d5
vitalets <vitalets@yandex-team.ru>
2016-08-29 22:31:38 +0300

    intercept http.createServer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 
 'use strict';
 
-// const fileserver = require('selenium-webdriver/lib/test/fileserver');
-const fileserver = require('./fileserver');
+/*
+ heroku cant start fileserver as is because it passes 'localhost' to .listen() method.
+ To overcome that with no edits of original httpserver.js we intercept http.createServer and exclude host
+ */
+
+const http = require('http');
+const _createServer = http.createServer;
+http.createServer = function () {
+  const server = _createServer.apply(http, arguments);
+  const _listen = server.listen;
+  server.listen = function (port, host, cb) {
+    // exclude host
+    return _listen.call(server, port, cb);
+  };
+  return server;
+};
+
+const fileserver = require('selenium-webdriver/lib/test/fileserver');
 
 fileserver.start(process.env.PORT || 2311).then(function() {
-    console.log('Server running at ' + fileserver.url());
+  console.log('Server running at ' + fileserver.url());
 });
-
 
 /*
 var express = require('express');
