32f0e65b2f266335e0527be9c503cd67e0e968e6
vitalets <vitalets@yandex-team.ru>
2016-08-29 21:35:51 +0300

    copy fileserver

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 
 'use strict';
 
-const fileserver = require('selenium-webdriver/lib/test/fileserver');
+// const fileserver = require('selenium-webdriver/lib/test/fileserver');
+const fileserver = require('./fileserver');
 
-/*
 fileserver.start(process.env.PORT || 2311).then(function() {
     console.log('Server running at ' + fileserver.url());
 });
-*/
 
+
+/*
 var express = require('express');
 var app     = express();
 
@@ -21,3 +22,4 @@ app.get('/', function(request, response) {
 }).listen(app.get('port'), function() {
   console.log('App is running, server is listening on port ', app.get('port'));
 });
+*/
