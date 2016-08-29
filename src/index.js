948725bfb0e2213bad3099bac5fb7b74d62b1453
vitalets <vitalets@yandex-team.ru>
2016-08-29 21:02:33 +0300

    add logging

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 
 const fileserver = require('selenium-webdriver/lib/test/fileserver');
 
+console.log('Starting server on ' + process.env.PORT);
+
 fileserver.start(process.env.PORT || 2311).then(function() {
     console.log('Server running at ' + fileserver.url());
 });
